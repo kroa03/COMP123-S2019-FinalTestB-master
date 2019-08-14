76d361a069b400e3190ae0b24d896c15b35a129d
301034220 <301034220@PR-E216-52>
2019-08-14 15:05:57 -0400

    Created Lists: FirstNameList, LastNameList Created Methods: LoadNames(), GenerateNames()

--- COMP123-S2019-FinalTestB/Views/CharacterGeneratorForm.cs
+++ COMP123-S2019-FinalTestB/Views/CharacterGeneratorForm.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+// For TextReader and TextWriter
+using System.IO;
 
 /*
  * STUDENT NAME: Koolait Roa
@@ -16,6 +18,12 @@ namespace COMP123_S2019_FinalTestB.Views
 {
     public partial class CharacterGeneratorForm : MasterForm
     {
+        public List<string> FirstNameList = new List<string>();
+        public List<string> LastNameList = new List<string>();
+
+        // Random object that would generate a random number
+        public Random random = new Random();
+
         public CharacterGeneratorForm()
         {
             InitializeComponent();
@@ -46,5 +54,55 @@ namespace COMP123_S2019_FinalTestB.Views
                 MainTabControl.SelectedIndex++;
             }
         }
+
+        /// <summary>
+        /// This method  loads the entire firstNames.txt and lastNames.txt file into FirstNameList and LastNameList lists
+        /// </summary>
+        public void LoadNames()
+        {
+            // textreader object to store the files
+            TextReader firstNamesFile = new StreamReader("..\\..\\Data\\firstNames.txt");
+            TextReader lastNamesText = new StreamReader("..\\..\\Data\\lastNames.txt");
+
+            // reads a line of characters from the text reader object
+            string line = firstNamesFile.ReadLine();
+
+            // loop that would read every line of the file and add each to the list
+            while (line != null)
+            {
+                FirstNameList.Add(line);
+                line = firstNamesFile.ReadLine();
+            }
+            FirstNameList.Add(line);
+            firstNamesFile.Close();
+
+
+            line = lastNamesText.ReadLine();
+            while (line != null)
+            {
+                LastNameList.Add(line);
+                line = lastNamesText.ReadLine();
+            }
+            LastNameList.Add(line);
+            lastNamesText.Close();
+        }
+
+        /// <summary>
+        ///  Method that will randomly pick First Names and Last Names from 
+        ///  the FirstNameList and LastNameList controls and set the 
+        ///  FirstNameDataLabel and LastNameDataLabel with these values 
+        /// </summary>
+        public void GenerateNames()
+        {
+            // gets the number of elements contained in the List
+            int listCount = FirstNameList.Count;
+            FirstNameDataLabel.Text = FirstNameList[random.Next(0, (listCount - 1))];
+            
+            listCount = LastNameList.Count;
+            string lname = LastNameList[random.Next(0, (listCount - 1))];
+            LastNameDataLabel.Text = lname;
+        }
+
+        
     }
 }
