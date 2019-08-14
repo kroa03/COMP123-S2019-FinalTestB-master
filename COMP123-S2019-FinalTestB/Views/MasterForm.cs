101bcff7e7a69698c07854f1fc70309ebc1ab983
301034220 <301034220@PR-E216-52>
2019-08-13 20:48:09 -0400

    Created forms

--- COMP123-S2019-FinalTestB/Views/MasterForm.cs
+++ COMP123-S2019-FinalTestB/Views/MasterForm.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * STUDENT NAME: Koolait Roa
+ * STUDENT ID: 301034220
+ * DESCRIPTION: This is the Character Generator  Form - the main form of the application
+ */
+
 namespace COMP123_S2019_FinalTestB.Views
 {
     public partial class MasterForm : Form
